0a4548d021cf3ba665cd26c555bc8447be224d4c
sen_gagan <sen1528gagan@gmail.com>
2024-01-06 23:42:09 +0530

    fix code

---
+++ server.js
@@ -0,0 +1,49 @@
+// const express = require("express");
+// const app = express();
+// const http = require('http').createServer(app)
+// const PORT = process.env.PORT || 3000;
+// http.listen(PORT,()=>{
+//     console.log(`listening on port ${PORT}`)
+// })
+// app.use(express.static(__dirname + '/public'))     //for image folder
+// //routes 
+// app.get('/',(req,res)=>{
+//     // res.send('hello world');
+//     res.sendFile(__dirname + '/index.html');
+// })  
+// // socket 
+// const io = require('socket.io')(http)
+// io.on('connection',(socket) => {
+//     console.log('connected..')
+//     socket.on('message',(msg)=>{
+//       socket.broadcast.emit('message',msg)
+//     })
+// })
+
+
+"use strict";
+
+const express = require("express");
+const app = express();
+const http = require('http').createServer(app);
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+app.use(express.static(__dirname + '/public')); // for image folder
+
+// routes
+app.get('/', (req, res) => {
+    // res.send('hello world');
+    res.sendFile(__dirname + '/index.html');
+});
+
+// socket
+const io = require('socket.io')(http);
+io.on('connection', (socket) => {
+    console.log('connected..');
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg);
+    });
+});
