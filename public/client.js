e5389295bf1824b75060ab87cb67f4031ca71658
sen_gagan <sen1528gagan@gmail.com>
2024-01-11 18:00:38 +0530

    fix one to one chat

--- public/client.js
+++ public/client.js
@@ -1,51 +1,62 @@
+// // grop
 
-// const socket = io()
-// let textarea = document.querySelector('#textarea')
-// let messageArea = document.querySelector('.message_area')
+// 'use strict';
+
+// const socket = io();
+// let textarea = document.querySelector('#textarea');
+// let messageArea = document.querySelector('.message_area');
 // let name;
 // do {
 //     name = prompt(`Please enter your name:`);
+//     console.log("name=",name);
 // } while (!name);
 // textarea.addEventListener('keyup', (e) => {
 //     if (e.key === 'Enter') {
-//         sendMessage(e.target.value)
+//         sendMessage(e.target.value);
 //     }
-// })
+// });
+
 // function sendMessage(message) {
 //     let msg = {
-//                 user: name,
-//                 message: message.trim(),
-//               }
+//         user: name,
+//         message: message.trim(),
+//     };
 
 //     // append
-//             appendMessage(msg, "outgoing")
-//             textarea.value = ''
-//             scrollToBottom()
+//     appendMessage(msg, 'outgoing');
+//     textarea.value = '';
+//     scrollToBottom();
 
 //     // send to server
-//             socket.emit('message', msg)
-// }
-// function appendMessage(msg, type) {
-//                     let mainDiv = document.createElement('div');
-//                     let className = type
-//                     mainDiv.classList.add(className, 'message')
-//                     let markup = `
-//                         <h4>${msg.user}</h4>
-//                         <p>${msg.message}</p>
-//                     `
-//                     mainDiv.innerHTML = markup
-//                     messageArea.appendChild(mainDiv)
-// }
-// socket.on('message', (msg) => {
-//                 // console.log(msg,"incoming");
-//                 appendMessage(msg, 'incoming')
-//                 scrollToBottom();
-// })
-// // scroll to bottom
-// function scrollToBottom() {
-//                  messageArea.scrollTop = messageArea.scrollHeight
+//     socket.emit('message', msg);
 // }
 
+// function appendMessage(msg, type) {
+//     let mainDiv = document.createElement('div');
+//     let className = type;
+//     mainDiv.classList.add(className, 'message');
+//     let markup = `
+//         <h4>${msg.user}</h4>
+//         <p>${msg.message}</p>
+//     `;
+//     console.log("markup=",markup);
+//     mainDiv.innerHTML = markup;
+//     messageArea.appendChild(mainDiv);
+// }
+
+// socket.on('message', (msg) => {
+//     console.log(msg,"incoming");
+//     appendMessage(msg, 'incoming');
+//     scrollToBottom();
+// });
+
+// // scroll to bottom
+// function scrollToBottom() {
+//     messageArea.scrollTop = messageArea.scrollHeight;
+// }
+
+
+//one to one chat
 
 'use strict';
 
@@ -53,10 +64,14 @@ const socket = io();
 let textarea = document.querySelector('#textarea');
 let messageArea = document.querySelector('.message_area');
 let name;
+
 do {
     name = prompt(`Please enter your name:`);
-    console.log("name=",name);
 } while (!name);
+
+let room = prompt(`Enter room number:`);
+socket.emit('joinRoom', room);
+
 textarea.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
         sendMessage(e.target.value);
@@ -69,35 +84,42 @@ function sendMessage(message) {
         message: message.trim(),
     };
 
-    // append
+    // Append the outgoing message without displaying it in the message area
     appendMessage(msg, 'outgoing');
+
+    // Clear the textarea and scroll to the bottom
     textarea.value = '';
     scrollToBottom();
 
-    // send to server
-    socket.emit('message', msg);
+    
+    // Emit the privateMessage event to send the message to the server
+    socket.emit('privateMessage', { room: room, msg: msg });
 }
 
 function appendMessage(msg, type) {
     let mainDiv = document.createElement('div');
     let className = type;
     mainDiv.classList.add(className, 'message');
-    let markup = `
-        <h4>${msg.user}</h4>
-        <p>${msg.message}</p>
-    `;
-    console.log("markup=",markup);
-    mainDiv.innerHTML = markup;
-    messageArea.appendChild(mainDiv);
+
+    // Check if the message is incoming or outgoing (from someone else or the current user)
+    if (type === 'outgoing' || (type === 'incoming' && msg.user !== name)) {
+        let markup = `
+            <h4>${msg.user}</h4>
+            <p>${msg.message}</p>
+        `;
+        mainDiv.innerHTML = markup;
+        messageArea.appendChild(mainDiv);
+    }
 }
 
 socket.on('message', (msg) => {
-    console.log(msg,"incoming");
+    // Display incoming messages in the message area
     appendMessage(msg, 'incoming');
     scrollToBottom();
 });
 
-// scroll to bottom
 function scrollToBottom() {
+    // Scroll the message area to the bottom
     messageArea.scrollTop = messageArea.scrollHeight;
 }
+
--- server.js
+++ server.js
@@ -1,53 +1,94 @@
+// // group chat
+// "use strict";
+
 // const express = require("express");
 // const app = express();
-// const http = require('http').createServer(app)
-// const PORT = process.env.PORT || 3000;
-// http.listen(PORT,()=>{
-//     console.log(`listening on port ${PORT}`)
-// })
-// app.use(express.static(__dirname + '/public'))     //for image folder
-// //routes 
-// app.get('/',(req,res)=>{
-//     // res.send('hello world');
+// const http = require('http').createServer(app);
+// const PORT = process.env.PORT || 3005;
+// const io = require('socket.io')(http);
+
+// http.listen(PORT, () => {
+//     console.log(`listening on port ${PORT}`);
+// });
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/index.html');
-// })  
-// // socket 
-// const io = require('socket.io')(http)
-// io.on('connection',(socket) => {
-//     console.log('connected..')
-//     socket.on('message',(msg)=>{
-//       socket.broadcast.emit('message',msg)
-//     })
-// })
+// });
+
+// io.on("connection", (socket) => {
+//     console.log("Connected to socket.io");
+
+//     socket.on("setup", (userData) => {
+//         socket.userData = userData;
+//         socket.join(userData._id);
+//         socket.emit("connected");
+//     });
+
+//     socket.on("join chat", (room) => {
+//         socket.join(room);
+//         console.log("User Joined Room: " + room);
+//     });
+
+//     socket.on("typing", (room) => socket.in(room).emit("typing"));
+
+//     socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+//     socket.on("new message", (newMessageReceived) => {
+//         var chat = newMessageReceived.chat;
+
+//         if (!chat.users) return console.log("chat.users not defined");
+
+//         chat.users.forEach((user) => {
+//             if (user._id == newMessageReceived.sender._id) return;
+
+//             socket.in(user._id).emit("message received", newMessageReceived);
+//         });
+//     });
+
+//     socket.on("disconnect", () => {
+//         if (socket.userData) {
+//             console.log("USER DISCONNECTED:", socket.userData._id);
+//             socket.leave(socket.userData._id);
+//         }
+//     });
+// });
 
 
-"use strict";
+// one to one chat
 
 const express = require("express");
 const app = express();
 const http = require('http').createServer(app);
-const PORT = process.env.PORT || 3005;
-http.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-});
+const io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public')); // for image folder
+app.use(express.static(__dirname + '/public'));
 
-// routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// socket
-const io = require('socket.io')(http);
-io.on('connection', (socket) => {
-    console.log('connected..');
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
-    socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg);
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+        // console.log(`User ${socket.id} joined room ${room}`);
     });
 
-    socket.on('disconnect',()=>{
-        console.log("disconnected...");
-    })
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+
+    socket.on('privateMessage', (data) => {
+         io.to(data.room).emit('message', data.msg);
+        // socket.broadcast.emit("message",data.msg);
+    });
 });
+
