df4cf59d3efa6ab6e8d3c82ca66ab38890b11a1e
sen_gagan <sen1528gagan@gmail.com>
2024-01-17 18:12:12 +0530

    fix code

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-// const url = "mongodb://127.0.0.1:27017/chatApplication";
+const url = "mongodb://chat-application-glki.onrender.com/chatApplication";
 
-const url = "https://chat-application-glki.onrender.com";
+// const url = "https://chat-application-glki.onrender.com";
 
 mongoose.connect(url);
 console.log("database connected successfully");
