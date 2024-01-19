309a9157bfa13e953138318bf4b940ed0925eab6
sen_gagan <sen1528gagan@gmail.com>
2024-01-19 14:31:39 +0530

    fix

--- server.js
+++ server.js
@@ -422,9 +422,9 @@ app.use('/API', userRouter);
 const publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
