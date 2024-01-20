0b37b0f9af5c3112c9d72640cc253ee3fd4c7f0c
sen_gagan <sen1528gagan@gmail.com>
2024-01-20 13:40:47 +0530

    fix

--- database/mysqlConnection.js
+++ database/mysqlConnection.js
@@ -1,23 +1,17 @@
-
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: '50.62.139.196',
     user: 'techiefreight',
     password: 'NCviwkO_;Wfx',
     database: 'techiefreight'
-    // host     : 'localhost',
-    // user     : 'root',
-    // password : '152897',
-    // database : 'customer'
 });
 
 connection.connect(function (error) {
-    console.log(error);
     if (error) {
-        console.log("database not connected",error);
+        console.log("Database connection failed:", error);
     } else {
-        console.log("database connected successfully",error);
+        console.log("Database connected successfully");
     }
 });
 
