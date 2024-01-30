9dca7e406916e3b50e4de8b4fe159f1a6b7ae10c
sen_gagan <sen1528gagan@gmail.com>
2024-01-30 17:16:28 +0530

    fix

--- database/mysqlConnection.js
+++ database/mysqlConnection.js
@@ -26,18 +26,18 @@ const dotenv = require('dotenv').config();
 
 // Use createPool instead of createConnection for better scalability
 const connection = mysql.createPool({
-    // host: process.env.HOST,
-    // user: process.env.USER,
-    // password: process.env.PASSWORD, // Use uppercase for consistency
-    // database: process.env.DATABASE,
-    // waitForConnections: true,
-    // connectionLimit: 10,
-    // queueLimit: 0
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD, // Use uppercase for consistency
+    database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 
-    host :"localhost",
-    user:"root",
-    password:"152897",
-    database:"techiefreight"
+    // host :"localhost",
+    // user:"root",
+    // password:"152897",
+    // database:"techiefreight"
    
 
 });
