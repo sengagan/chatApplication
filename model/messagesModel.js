b104fdf3b2a29bc90cc33f13cf92d18e82b2f163
sen_gagan <sen1528gagan@gmail.com>
2024-02-07 16:22:47 +0530

    fix

--- model/messagesModel.js
+++ model/messagesModel.js
@@ -222,7 +222,7 @@ const save = async (details) => {
 
 
 const getData = async (data) => {
-    console.log("getdata/model-==-");
+    console.log("getdata/model-==-",data);
 
     let query = `SELECT * FROM messages 
               WHERE (fromUserId = ${data.sender_id} AND toUserId = ${data.receiver_id})
